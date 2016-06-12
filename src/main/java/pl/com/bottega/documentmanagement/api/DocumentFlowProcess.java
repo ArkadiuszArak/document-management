ba5d2613ee90857ebd614bc2937802ae206ef8ae
arkadiuszarak <arak.arkadiusz@gmail.com>
2016-06-12 15:27:20 +0200

    API methods

--- src/main/java/pl/com/bottega/documentmanagement/api/DocumentFlowProcess.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/DocumentFlowProcess.java
@@ -2,32 +2,44 @@ package pl.com.bottega.documentmanagement.api;
 
 import pl.com.bottega.documentmanagement.domain.DocumentNumber;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Created by arkadiuszarak on 12/06/2016.
  */
 public class DocumentFlowProcess {
 
-    public DocumentNumber create(String titles, String content){
+    public DocumentNumber create(String title, String content){
+        checkNotNull(title);
+        checkNotNull(content);
+
         return null;
     }
 
     public void change(DocumentNumber documentNumber, String newTitle, String newContent){
+        checkNotNull(documentNumber);
+        checkNotNull(newContent);
 
     }
 
     public void verify(DocumentNumber documentNumber){
+        checkNotNull(documentNumber);
 
     }
 
     public void publish(DocumentNumber documentNumber){
+        checkNotNull(documentNumber);
+
 
     }
 
     public void archive(DocumentNumber documentNumber){
+        checkNotNull(documentNumber);
 
     }
 
     public DocumentNumber createNewVersion(DocumentNumber documentNumber){
+        checkNotNull(documentNumber);
         return null;
     }
 
