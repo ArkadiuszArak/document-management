45e30a26479a1cf7489c0db87737abbf9f95bfc2
arkadiuszarak <arak.arkadiusz@gmail.com>
2016-06-12 11:08:04 +0200

    My first Git commit

---
+++ src/main/java/pl/com/bottega/documentmanagement/api/HelloWorld.java
@@ -0,0 +1,10 @@
+package pl.com.bottega.documentmanagement.api;
+
+/**
+ * Created by arkadiuszarak on 12/06/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
