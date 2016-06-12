b91b0b65714d2dde5653ddc0c1a06a23fd7364ee
arkadiuszarak <arak.arkadiusz@gmail.com>
2016-06-12 15:32:38 +0200

    API methods validations not null

--- src/main/java/pl/com/bottega/documentmanagement/api/DocumentsCatalog.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/DocumentsCatalog.java
@@ -3,15 +3,19 @@ package pl.com.bottega.documentmanagement.api;
 import pl.com.bottega.documentmanagement.domain.Document;
 import pl.com.bottega.documentmanagement.domain.DocumentNumber;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Created by arkadiuszarak on 12/06/2016.
  */
 public class DocumentsCatalog {
     public Document get(DocumentNumber documentNumber){
+        checkNotNull(documentNumber);
         return null;
     }
 
     public Iterable<Document> find(DocumentCriteria documentCriteria){
+        checkNotNull(documentCriteria);
         return null;
     }
 
